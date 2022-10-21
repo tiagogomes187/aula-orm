8aeb52f6b5e88387073668d55c0920852890d8b1
Tiago Gomes <tiagogomes187@gmail.com>
2022-10-20 22:46:18 -0300

    Project created

---
+++ src/main/java/com/github/tiagogomes187/aulaorm/AulaormApplication.java
@@ -0,0 +1,13 @@
+package com.github.tiagogomes187.aulaorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tiagogomes187/aulaorm/AulaormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tiagogomes187.aulaorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
